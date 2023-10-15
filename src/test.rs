2ffaa200e2012c46f740f147f2401bb2c6768095
lolpro11 <lolpro11lolx@gmail.com>
2023-10-15 06:59:27 -0700

    changes

--- src/test.rs
+++ src/test.rs
@@ -10,11 +10,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     for entry in fs::read_dir(dir)? {
         let entry = entry?;
         let path = entry.path();
-        println!("{}", entry.file_name().to_str().unwrap());
+        let mut csv_str: String; //= entry.file_name().to_str().unwrap().to_string();
         if path.is_file() && path.extension().unwrap_or_default() == "json" {
             let json = fs::read_to_string(&path)?;
-            let feed: Root = serde_json::from_str(&json)?;
+            let domain: Root = serde_json::from_str(&json)?;
+            for feed in domain.feeds.unwrap() {
+                //csv_str.push_str(format!(",{},{}", feed.id.as_str(), feed.spec.as_str()).as_str());
+                if feed.spec == "gtfs-rt" {
+                    if feed.authorization.is_some() {
+                        println!("{},TRUE", feed.id);
+                    } else {
+                        println!("{},FALSE", feed.id);
+                    }
+                    
+                }
+            }
         }
+        //println!("{}", csv_str);
     }
     Ok(())
 }
